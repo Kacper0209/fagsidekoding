15074a69b606bc5737a4a11ffa7bf138270cd872
sommervold <nicolai@sommervold.net>
2018-01-12 07:25:22 +0100

    Update session.go

--- Backend/session.go
+++ Backend/session.go
@@ -1,50 +1,54 @@
 package main
 
 import (
-	"log"
-	"github.com/satori/go.uuid"
+	"fmt"
 	"net/http"
+	"gopkg.in/mgo.v2/bson"
 
 )
 
 func getUser(w http.ResponseWriter, req *http.Request) user {
 	c, err := req.Cookie("session")
+
 	if err != nil{
-		sID, err := uuid.NewV4()
+		sID := bson.NewObjectId()
 		if err != nil {
-			log.Println(err)
+			fmt.Println(err)
 		}
 		c = &http.Cookie{
 			Name: "session",
-			Value: sID.String(),
+			Value: sID.Hex(),
 		}
 	}
-	http.SetCookie(w, c)
-
 	var u user
-	if un, ok := dbSessions[c.Value]; ok {
-		u = dbUsers[un]
+	http.SetCookie(w, c)
+	var ValidSession ssession
+	db := session.DB("fagside").C("Sessions")
+	if err := db.FindId(bson.ObjectIdHex(c.Value)).One(&ValidSession); err != nil {
+	    return u
+	}
+	db = session.DB("fagside").C("Users")
+	if err := db.Find(bson.M{"UserName":ValidSession.Un}).One(&u); err != nil {
+	    return u
 	}
 
 	return u
 }
 
 func alreadyLoggedIn(req *http.Request) bool {
-
+	var ValidSession ssession
+	var result []ssession
 	c, err := req.Cookie("session")
 	if err != nil {
-
 		return false
 	}
-	un := dbSessions[c.Value]
-	_, ok := dbUsers[un]
-	
-	return ok
+	db := session.DB("fagside").C("Sessions")
+	err = db.Find(nil).Sort("-_id").Limit(50).All(&result)
+    	if err != nil {
+   			fmt.Println(err)
+    	}
+	if err := db.FindId(bson.ObjectIdHex(c.Value)).One(&ValidSession); err != nil {
+	    return false
+	}
+	return true
 }
-// func cleanSessions() {
-// 	for k, v := range dbSessions {
-// 		if time.Now().Sub(v.lastActivity) > (time.Minute * 30) {
-// 			delete(dbSessions, k)
-// 		}
-// 	}
-// }
